57d84eef900cb183098330d3558cc382e5fd8e90
Yash <yash069@live.com>
2014-12-20 18:04:23 +0000

    additions

---
+++ definitions/lighttpd_module.rb
@@ -0,0 +1,12 @@
+define :lighttpd_module, :enable => true do
+	module_command = (params[:enable]) ? "lighttpd-enable-mod" : "lighttpd-disable-mod"
+	include_recipe "lighttpd"
+	bash "run_lighty_mod" do
+		user "root"
+		code <<-EOH
+			#{module_command} #{params[:name]}
+		EOH
+		return [0, 2]
+		notifies :restart, resource(:service => "lighttpd"), :delayed
+	end
+end
--- recipes/php.rb
+++ recipes/php.rb
@@ -5,5 +5,15 @@ node[:php][:packages].each |package_name| do
 end
 
 service "php5-fpm" do
+	provider Chef::Provider::Service::Upstart
+	supports :status => true, :restart => true, :reload => true
+	action [ :enable, :start ]
+end
 
+cookbook_file "/etc/php5/fpm/php.ini"
+	source "php.ini"
+	owner 'root'
+	group 'root'
+	mode '0644'
+	notifies :reload, "servuce[php-fpm]", :immediately
 end
