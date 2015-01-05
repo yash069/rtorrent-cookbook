4a6bd6f3ad7736c7e8e1be9be706ab828904f959
Yash <yash069@live.com>
2015-01-05 18:23:08 +0000

    update

--- recipes/vsftpd.rb
+++ recipes/vsftpd.rb
@@ -8,20 +8,6 @@ service "vsftpd" do
 	action [ :enable, :start ]
 end
 
-bash "get_public_ip" do
-	user 'root'
-	code <<-EOH
-	curl "http://whatismyip.akamai.com/" > "/tmp/myip"
-	EOH
-end
-
-ruby_block "set_attribute" do
-  block do
-    node.normal['public_ip'] = `head -1 /tmp/myip`
-  end
-  action :create
-end
-
 template "/etc/vsftpd.conf" do
 	source "vsftpd.conf.erb"
 	owner 'root'
